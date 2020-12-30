dfb2e77ebf9ef98233aef4762ecb65c20560dc22
Ricardo Salas <ricardo.salas.cg@gmail.com>
2020-12-30 13:02:41 -0300

    Initial commit

---
+++ lumina.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("home.html", title = "Home")
+
+@app.route("/register")
+def register():
+    return render_template("register.html", title = "Register")
+
+@app.route("/login")
+def login():
+    return render_template("login.html", title = "Login")
+
+@app.route("/project")
+def project():
+    return render_template("project.html", title = "Project")
+
+@app.route("/loadProject")
+def loadProject():
+    return render_template("loadProject.html", title = "Load Project")
+
+@app.route("/catalogue")
+def catalogue():
+    return render_template("catalogue.html", title = "Catalogue")
